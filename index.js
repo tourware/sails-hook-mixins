4f7c247f5c8258f320209fff01a362302b347dec
Simon Milz <smilz@nvidia.com>
2017-01-04 16:50:58 +0100

    added init function ot get access to the model

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ module.exports = function(sails) {
 
 					_.each(model.mixins, function (name) {
 						var mixin = require(process.cwd() + '/api/mixins/' + name);
-						deepExtend(model, mixin);
+						deepExtend(model.attributes, mixin.attributes);
+
+						if (typeof mixin.init === 'function') {
+							mixin.init(model);
+						}
 					});
 				});
 
